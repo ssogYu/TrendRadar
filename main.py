242e63470f1d08cd0b8b1b271b5b4da5a5379d48
shunsong.yu <shunsong.yu@ly.com>
2025-11-14 10:37:01 +0800

    feat:update

--- main.py
+++ main.py
@@ -2313,7 +2313,7 @@ def render_html_content(
             
             <div class="footer">
                 <div class="footer-content">
-                    由 <span class="project-name">TrendRadar</span> 生成 · 
+                    由 <span class="project-name">余～</span> 生成 · 
                     <a href="https://github.com/sansan0/TrendRadar" target="_blank" class="footer-link">
                         GitHub 开源项目
                     </a>"""
